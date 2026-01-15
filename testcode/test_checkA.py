ae64244517ab6a5043f1c6fe7057d429238bd7a1
MotoyaTakashi <tamotoya0077@gmail.com>
2026-01-15 17:20:17 +0900

    update

--- testcode/test_checkA.py
+++ testcode/test_checkA.py
@@ -2,66 +2,66 @@ from src.exec_check import checkA
 
 def test_contains_A():
 
-    # "A" ���܂ޏꍇ
+    # "A" を含む場合
 
-    # (��) assert �Ƃ����̂� �������茾����
-    # �ucheckA("A") �����s���Č��ʂ� 1 �ɂȂ邩�m�F���āv�̂悤�ȋ@�\�ł��B
-    # ���K�ł́u���ʂ��t�@�C���ɏo�͂��āA�����f�[�^�Ɣ�r����v�̂悤��
-    # �b���������Ǝv���܂����A����ɊY�����邱�Ƃ� pytest �ł���Ă������̂ł��B
+    # (※) assert というのは ざっくり言うと
+    # 「checkA("A") を実行して結果が 1 になるか確認して」のような機能です。
+    # 演習では「結果をファイルに出力して、正解データと比較する」のような
+    # 話をしたかと思いますが、それに該当することを pytest でやってくれるものです。
 
-    # �P�̕���
+    # 単体文字
     assert checkA("A") == 1
 
-    # �擪
+    # 先頭
     assert checkA("APPLE") == 1
 
-    # �r��
+    # 途中
     assert checkA("TABLE") == 1
 
-    # �Ō�
+    # 最後
     assert checkA("CUBA") == 1
 
-    # ����
+    # 複数
     assert checkA("BANANA") == 1
 
 
 
 def test_contains_B():
 
-    # "B" ���܂ޏꍇ
-    # �� ���K�Ȃ̂ŒP�����̂��ߏȗ����Ă܂����A���H�ł� "A" �̏ꍇ�Ɠ��l�̔M�ʂŃe�X�g�P�[�X������Ă��������B
+    # "B" を含む場合
+    # ※ 演習なので単純化のため省略してますが、実践では "A" の場合と同様の熱量でテストケースを作ってください。
 
-    # �P�̕���
+    # 単体文字
     assert checkA("B") == 2
 
 
 
 def test_contains_C():
 
-    # "C" ���܂ޏꍇ
-    # �� ���K�Ȃ̂ŒP�����̂��ߏȗ����Ă܂����A���H�ł� "A" �̏ꍇ�Ɠ��l�̔M�ʂŃe�X�g�P�[�X������Ă��������B
+    # "C" を含む場合
+    # ※ 演習なので単純化のため省略してますが、実践では "A" の場合と同様の熱量でテストケースを作ってください。
 
-    # �P�̕���
+    # 単体文字
     assert checkA("C") == 3
 
 
 
 def test_does_not_contain_abc():
-    # "A", "B", "C" ���܂܂Ȃ��ꍇ
+    # "A", "B", "C" を含まない場合
     assert checkA("DFGHIJK") == 0
     assert checkA("123") == 0
-    assert checkA("") == 0  # �󕶎�
+    assert checkA("") == 0  # 空文字
 
 
 
 def test_case_sensitivity():
-    # �������� "a" �͊܂܂Ȃ��Ɣ��肳��邱�Ƃ��m�F�i�啶���������̋�ʁj
+    # 小文字の "a" は含まないと判定されることを確認（大文字小文字の区別）
     assert checkA("apple") == 0
 
-    # �������� "b" �͊܂܂Ȃ��Ɣ��肳��邱�Ƃ��m�F�i�啶���������̋�ʁj
+    # 小文字の "b" は含まないと判定されることを確認（大文字小文字の区別）
     assert checkA("b") == 0
 
-    # �������� "c" �͊܂܂Ȃ��Ɣ��肳��邱�Ƃ��m�F�i�啶���������̋�ʁj
+    # 小文字の "c" は含まないと判定されることを確認（大文字小文字の区別）
     assert checkA("c") == 0
 
 
